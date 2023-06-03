b64a764e2ea28ba72f52635bd725beff9bcc2f2d
Abhinav TV <abhinavgit8@gmail.com>
2023-06-03 15:00:21 +0530

    some chnages are there

--- Components/Dashboard/Dashboard.tsx
+++ Components/Dashboard/Dashboard.tsx
@@ -1,10 +1,39 @@
-import { Grid } from '@mui/material'
+import { Box, Grid, Typography } from '@mui/material'
+import { useRouter } from 'next/navigation'
 import React from 'react'
+import TableUi from '../UI/TableUi/TableUi'
 
 export default function Dashboard() {
+
+  const router = useRouter()
+
   return (
-    <Grid>
-        Dashboard
-    </Grid>
+    <Grid container>
+
+      <Grid container>
+
+        <Box bgcolor="red" onClick={() => router.push('/dashboard/create')} sx={{
+          ml: 2,
+          my: 2,
+          cursor: 'pointer'
+        }}>
+
+          <Typography variant='h5' sx={{
+            px: 2,
+            py: 1
+          }}> CREATE </Typography>
+
+        </Box>
+
+      </Grid>
+
+
+
+
+
+
+      <TableUi />
+
+    </Grid >
   )
 }
